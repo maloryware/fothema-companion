0d9e434f4a8373150a13e672801ed44cafd01629
maloryware <mal.larsson.reyes@gmail.com>
2024-12-02 22:40:57 +0000

    refactored the stupid bullshit i had in page_add.dart

--- lib/pages/page_add.dart
+++ lib/pages/page_add.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:fothema_companion/bluetooth.dart';
 import 'package:universal_ble/universal_ble.dart';
 
+import '../widget/device_tile.dart';
+
 class AddDevicePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _AddDevicePageState();
@@ -91,86 +93,13 @@ class _AddDevicePageState extends State<AddDevicePage>{
                   child: ListView(
                     children: [
                         for(var device in devices)
-                          ExpansionTile(
-                            leading: Icon(Icons.bluetooth),
-                            title: Text(
-                                device.name != null && device.name.toString().isNotEmpty ? device.name.toString() : device.deviceId,
-                                style: connectedDevices.contains(device) ? TextStyle(color: Colors.green) : TextStyle()),
-                            subtitle: Text("Connectivity: ${145 + device.rssi! < 100 ? 145 + device.rssi! : 100}%"),
-
-                            //* Contents of the expanded item go below
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(left: 5),
-                                child: Align(alignment: Alignment.centerLeft, child: Text(
-                                    connectedDevices.contains(device) ? "Connected" :
-                                    device.isPaired! ? "Paired" : "Not paired" )
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  Padding(padding: EdgeInsets.only(left: 5)),
-                                  ElevatedButton.icon(
-                                    onPressed: connectedDevices.contains(device) ? null : () => UniversalBle.connect(device.deviceId),
-                                    icon: Icon(Icons.play_arrow_outlined),
-                                    label: Text("Connect")
-                                  ),
-
-                                  ElevatedButton.icon(
-                                    onPressed:  device.isPaired! ? null : () => UniversalBle.pair(device.deviceId),
-                                    icon: Icon(Icons.connect_without_contact),
-                                    label: Text("Pair")
-                                  ),
-                                  ElevatedButton.icon(
-                                    onPressed: () => print(device.toString()),
-                                    icon: Icon(Icons.bug_report),
-                                    label: Text("Print device info")
-                                  ),
-                                ]
-                              ),
-                            ]
-                          ),
+                          DeviceTile(device: device, title: device.name != null && device.name.toString().isNotEmpty ? device.name.toString() : device.deviceId),
                       if(hiddenDevices.isNotEmpty)
                         ExpansionTile(
                           title: Text("Hidden devices"),
                           children: [
                             for(var device in hiddenDevices)
-                              ExpansionTile(
-                                  leading: Icon(Icons.bluetooth),
-                                  title: Text(device.name != null && device.name.toString().isNotEmpty ? device.name.toString() : device.deviceId),
-                                  subtitle: Text("Connectivity: ${145 + device.rssi! < 100 ? 145 + device.rssi! : 100}%"),
-                                  //* Contents of the expanded item go below
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 5),
-                                      child: Align(alignment: Alignment.centerLeft, child: Text(
-                                          connectedDevices.contains(device) ? "Connected" :
-                                          device.isPaired! ? "Paired" : "Not paired" )
-                                      ),
-                                    ),
-                                    Row(
-                                        children: [
-                                          Padding(padding: EdgeInsets.only(left: 5)),
-                                          ElevatedButton.icon(
-                                              onPressed: connectedDevices.contains(device) ? () => UniversalBle.disconnect(connectedDevices.first.deviceId) : () => UniversalBle.connect(device.deviceId),
-                                              icon: Icon(Icons.play_arrow_outlined),
-                                              label: Text(connectedDevices.contains(device) ? "Disconnect" : "Connect")
-                                          ),
-
-                                          ElevatedButton.icon(
-                                              onPressed:  device.isPaired! ? null : () => UniversalBle.pair(device.deviceId),
-                                              icon: Icon(Icons.connect_without_contact),
-                                              label: Text("Pair")
-                                          ),
-                                          ElevatedButton.icon(
-                                              onPressed: () => print(device.toString()),
-                                              icon: Icon(Icons.bug_report),
-                                              label: Text("Print device info")
-                                          ),
-                                        ]
-                                    ),
-                                  ]
-                              ),
+                              DeviceTile(device: device, title: "Hidden devices")
                           ],
                         )
 
